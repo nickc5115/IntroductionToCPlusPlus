1cf8d0db1c2d1d28530a44669077d9a504d2cb23
Nick Carfagno <--global>
2022-10-20 14:57:00 -0400

    finished the bmi thing

--- src/cpp/bmiimperial.cpp
+++ src/cpp/bmiimperial.cpp
@@ -3,18 +3,20 @@
 
 const double KG_PER_POUND = 0.453592;
 const double M_PER_INCH = 0.0254;
-int main()
-{
-    double height;
-    double weight;
-    
-    std::cout << "Enter height in inches:" << std::endl;
-    std::cin >> height;
+int main() {
+  double height;
+  double weight;
 
-    std::cout << "Enter weight in pounds:" << std::endl;
-    std::cin >> weight;
+  std::cout << "Enter height in inches:" << std::endl;
+  std::cin >> height;
 
-    height = pow(height, 2);
-    std::cout <<"BMI is: " << weight/height << "!";
+  std::cout << "Enter weight in pounds:" << std::endl;
+  std::cin >> weight;
+  double heightM = height * M_PER_INCH;
+  double weightKg = weight * KG_PER_POUND;
 
+  heightM = pow(heightM, 2);
+  std::cout << "BMI is: " << weightKg / heightM << "!";
+
+  exit(0);
 }
