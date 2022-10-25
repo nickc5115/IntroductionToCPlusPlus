50762fbc77f864cb3592ee8ab6fd5b76fcbb32bd
nick <nicholas.a.carfagno@gmail.com>
2022-10-25 18:07:16 -0400

    adding factorial program

---
+++ src/cpp/kcombinations/kcombinations.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int num);
+int kCombinations(int n, int k);
+
+int main(){
+    int n, k, k_comb;
+    
+
+    cout<<"Please enter n and k values (n>=k):"<<endl;
+    cin>>n>>k;
+
+    k_comb = kCombinations(n, k);
+
+    cout<<n<<" choose "<<k<<" is ";
+    cout<<k_comb<<endl;
+
+    return 0;
+}
+
+int kCombinations(int n, int k){
+    int nFact, kFact, n_kFact;
+
+    nFact = factorial(n);
+    kFact = factorial(k);
+    n_kFact = factorial(n-k);
+
+    return (nFact / (kFact*n_kFact));
+}
+int factorial(int num) {
+    int factRes, i;
+    factRes = 1;
+    for(i=1; i<=num; i++){
+        factRes *= i;
+    }
+    return factRes;
+}
